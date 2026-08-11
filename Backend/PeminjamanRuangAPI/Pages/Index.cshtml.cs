420e2a0cdaa1e0d6a687221b4a62d3e1b0d7ac85
Choqie Alfaro <choqie.alfaro@gmail.com>
2026-08-11 14:29:59 +0700

    Menambahkan webapp

---
+++ Backend/PeminjamanRuangAPI/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PeminjamanRuangAPI.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
